bb3872a5128511fc232f4cfe980390c64062113a
unknown <sobatsubihi@163.com>
2018-02-24 17:29:24 +0800

    asd

--- src/components/novel2.vue
+++ src/components/novel2.vue
@@ -38,7 +38,7 @@
 				<div id="main-text">
 				<!--<p>{{index1}}</p>-->
 				<!--<textarea>{{chapter.content}}</textarea>-->
-				<p  id="p" v-html="content"></p>
+				<p v-html="content"></p>
 					<!--<p>Dragon Mountain Calendar, Year 9616. Winter. Tranquil Sun Province, Azure River County, within the Water Rites Town. A boy about seven or eight years old dressed in exquisitely tailored clothes made of white fur dashed dashed through the mountains with a bag on his back. His red lips parted in smile to reveal his pearly teeth. He held a spear of black wood in his right hand. Snow gently drifted off the rustled leaves from the surrounding trees as he pursued a terror-stricken wild deer. "Alright!" The boy hoisted up the short spear. His body tilted slightly backwards, strength flowing from his waist to his right arm as he threw the spear! Whoosh! The short spear whistled through the air, piercing a few tree leaves as it covered a distance of 30 meters, just barely scraping past the rear of the wild deer. Only a speck of blood remained on the deer’s butt after it landed in the snow. The wild deer accelerated with all of its might towards the depths of the mountains. But just as it was about to run out of sight… A rock flew by. The rock flew through the through the mountain forest like a laser, instantly closing the 100 meter gap. With a thump, it pierced through the trunk of a large tree and then the deer’s skull. The wild deer swayed from side to side as its momentum carried it forward ten more meters before crashing into the ground. "Father!" The boy turned his head and looked into the distance. "Don’t help me. A little more and I would have got it." "That deer would have escaped if I didn’t help. Your spear’s accuracy while running is still inadequate. Tonight, go back and practice using the spear another 500 times." The forceful voice resonated from far away. Two figures appeared in the distance.. One was a rather sturdy middle-aged man with black hair and black eyes who carried a box of weapons on his back. The other was even bulkier, towering at over two meters in height. His arms were thicker than human thighs, his head was that of a lion. He truly did have a lion’s head and a human’s body! His messy blonde hair clearly marked him as one of the seldom seen ‘Lionmen’ of the beastman race. He also carried a box. The middle-aged man smiled, "Tong San, old friend, you see how great my son is? He’s only eight yet he’s already as strong as a grown man." "Mn. Xue Ying is not bad. It won’t be a problem for him to surpass you in the future," Tong San teased. "Of course he’ll be stronger than me. When I was eight, I was still laughing around and playing with the village’s children. I didn’t understand anything at all. I only got a chance to start cultivating qi after entering the army!" The middle-aged man sighed in regret. "I may not be able to provide my son with the best conditions for training, but as his father, I will spare no effort to raise him properly!" "Dong Bo, it’s already impressive that you’ve become a Heaven Rank Knight after starting off as a commoner and purchased your own territory to become a lord." The brawny lion-man smiled. This middle-aged male was the lord of a territory of more than 100 square kilometers: Baron Dong Bo Lie! Baron was the lowest title of nobility in the Xia Clan’s Mountain Dragon Empire. When the empire had first been established, the awarding of noble titles was very strict. The current empire had already existed for 9000 years. This goliath had now fallen to the point where the government permitted low rank noble titles to be bought and sold. Dong Bo Lie and his wife had decided to buy a noble title only because they had a child. After buying a piece of land, they had named it Snow Eagle Territory after their son!It was clear how much they loved him. Of course, this was only a small piece of land in Water Rites County. "I only started cultivating Qi when I was twenty years old, but my son is different. He’s only eight right now. I estimate that once he’s around ten years old he can begin cultivating Qi." Dong Bo Lie roared with laughter. "He will definitely surpass me." Dong Bo Lie looked at his son with fatherly love and expectation. "Considering what I’ve seen, ten years old sounds about right." The brawny lion-man also approved. With all their life experience, their judgement was surely accurate. "Father, you could actually throw a rock through a tree from so far away?" The boy stood next to the tree. He tried to hug it, but was unable to completely wrap his arms around it. Its trunk had a massive hole. "It would take forever for me to cut down a tree this thick." "Now you know the power of a Heaven Rank Knight," Tong San said. Dong Bo Lie chuckled. As a father, he still liked to show off in front of his son. "As powerful as a god?" The boy deviously grinned. "God?" Dong Bo Lie and the Tong San went silent. The Dragon Mountain Empire’s founder, the Dragon Mountain Emperor, was a formidable god. This was common knowledge, almost everyone in this world knew this. Dong Bo Lie could be considered a brave individual within the army, but compared to a god? There was simply no comparison. "It seems practicing 500 times is too little for you tonight. Mn. Let’s add one another 1000" Dong Bo Lie licked his lips. "Father!" The boy opened his eyes wide. "You, you..." "Looks like you still dare to argue with me! Remember, when arguing with your father, you will definitely suffer losses. Alright, now go back, go back." Dong Bo Lie said. Tong San took out a flute, put it in his mouth, and played a few low notes. The sound spread throughout the mountains. Soon, twenty armored soldiers rapidly rushed over. "Take the deer back." Dong Bo Lie commanded. "Yes, m’lord." The soldiers respectfully responded. Dong Bo Lie and Tong San brought Xue Ying to the peak of the mountain. A large number of horses and approximately 100 soldiers were gathered here. A mysterious purple-gowned lady was seated on a blanket atop the snow. Next to the purple-gowned lady was a child about two or three years old jumping around. The soldiers looked at the purple-gowned lady with reverence. This purple-gowned lady was a powerful mage! "Pebble, come look who’s here," the lady said. The two or three year old child immediately turned his head to look, eyes brightening. "Carry me, brother. Carry me, brother." The child turned around and ran over to Xue Ying. The lady also smiled at this scene. "Pebble." Xue Ying walked forward and crouched down. His younger brother Qing Shi threw himself into his embrace. "Carry me, brother. Carry me, brother!" Xue Ying carried his younger brother and kissed him. "Pebble, I hunted a deer today. Look." Xue Ying pointed at the deer the soldiers carried. "Veer? Veer?" Qing Shi opened his crow-black eyes wide as his mouth let out an unclear sound. Dong Bo Qing Shi was only two years old. Try as he might to speak, his words were still unclear and didn’t understand the meaning. "It’s a deer, a type of animal found on our family’s mountain." "Xue Ying, why don’t you give me your little brother." The purple-gowned lady also got up and walked over. "Yes, mother." Xue Ying handed over his little brother. The purple-robed lady said, "I brought some sweet olive cakes. They’re in the basket and should still be hot. Why don’t you hurry up and eat them?" "Cake?" Xue Ying’s eyes brightened and his mouth watered. Realizing that he was drooling, he immediately dashed over. "I want to eat! I want to eat!" Qing Shi immediately struggled from his mother’s embrace. He became very energetic whenever cake was mentioned. Actually, he was quite disobedient when eating. "Of course there’s some for you too, you little glutton." The purple-robed lady saw Dong Bo Lie and Tong San, "You two hurry up too. I made some food for you to eat as well." "Ha ha ... Master is not only powerful in magic, but also in the kitchen," said the brawny lion-man. Tong San had once been a slave when he was little, and had gone from being the purple-gowned lady’s servant to one of her followers. Even though many years had passed, and their relationship had grown to become like that of family, Tong San Insisted on calling her "Master." ... Once Xue Ying was full, he looked into the distance. From the top of the mountain, he could see a few mountains and some farmland with a single glance. Everywhere he looked was his family’s territory. His mother and father had stopped their days of adventuring when he was born, and instead, purchased a title of nobility and a large piece of land. The entire strip of land had been titled Snow Eagle Territory! Dong Bo Xue Ying took a long stretch, his face full of happiness. He had a mother and father who loved him dearly. He had a cute little brother. He had the many kind people of his territory. With this type of life, Dong Bo Xue Ying was extremely satisfied. The only thing that gave him a headache was that his father’s training was too painful. "Practicing 1000 more times on top of the 500… and I still need to practice my spear techniques..." Dong Bo Xue Ying’s face turned sour. ****** Nightfall, under the waning moon. The wind whistled. Boom! A thousand meters above the ground, a giant bird flew quickly, resembling like a black cloud. This giant bird had four wings and a wingspan of over twenty meters. Its flying speed surpassed the speed of sound. It was the extremely tyrannical and terrible demon beast, the Four-Winged Vulture. Two figures sat cross-legged on its back. One was a man in silver armor, while the other was robed in grey and carried a purple staff. "Where are we?" asked the grey-robed person. "Master, we have already entered Water Rites County. We should arrive at Snow Eagle Territory in about half an hour." The silver-armoured man lowered his head, clearly distinguishing his lower position. "I’ll finally see my sister again in half an hour." The grey-robed person’s voice carried heavy emotions. "You really are quite capable of hiding. Even when pursued by our family, you were able to hide for more than fifteen years..." In the darkness of the night, the Four-Winged Vulture headed straight for Snow Eagle Territory.</p>-->
 				</div>
 				<div id="main-bottom">
@@ -149,31 +149,25 @@
 				chapterindex: 1,//章节数
 				chaptercontent: "",//显示内容
 				pages:"",
-				chapter:"",
 				chapter_id: "",
-				top:"",
-				content:""
+				top:""
 			}
 		},
 
 		mounted() {
 		  this.width();
-		  this.br();
 		  //this.request();
 		  //this.topages();
 		},
 		created() {
 			this.getBookId();
 			console.log(this.detailId)
-			this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:1},
+			this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:1},
 			{emulateJSON:true})
 			.then(res => {
 			this.list = (res.body.ResultDate.bookInfo);
 			this.chapter = (res.body.ResultDate.chapterInfo);
-			this.content = (res.body.ResultDate.chapterInfo.content).replace(/\n/g, "<br/>");
-			this.chapter=this.chapter.content.replace(/\n/g, "<br/>");
-
-			
+			this.content=this.chapter.content.replace(/\n/g, "<br/>");	
 			this.detailId =(res.body.ResultDate.bookInfo.id);
 
 			
@@ -183,7 +177,8 @@
 //			this.next = (res.body.ResultDate.chapterInfo.next_chapter_id);
 //			console.log(this.pre)
 ;
-			},err => {
+				},err => {
+			//alert('wrong');
 				console.log('err');		
 			});
 			//推荐列表
@@ -204,15 +199,9 @@
 			//模拟接口取数据，这里直接赋值
 			//this.pages();
 //			this.chaptercontent = this.chapterlist[0];
-//			this.index1 = this.chaptercontent;	
+//			this.index1 = this.chaptercontent;
 		},
 		methods: {
-			br:function(chapter){
-				this.content = document.getElementById('p').innerText;
-//				this.chapter.content.replace(/\n|\r\n/g,"<br/>");
-				console.log(this.chapter)
-			},
-			
 			//推荐列表
 			//第一本
 			recom:function(){
@@ -268,7 +257,7 @@
 			},
 			//章节
 			chapters:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:1},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:1},
 				{emulateJSON:true})
 				.then(res => {
 	//			this.list = (res.body.ResultDate.bookInfo);
@@ -281,7 +270,7 @@
 				});
 			},
 			chapters2:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:2},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:2},
 				{emulateJSON:true})
 				.then(res => {
 	//			this.list = (res.body.ResultDate.bookInfo);
@@ -294,7 +283,7 @@
 				});
 			},	
 			chapters3:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:3},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:3},
 				{emulateJSON:true})
 				.then(res => {
 	//			this.list = (res.body.ResultDate.bookInfo);
@@ -307,7 +296,7 @@
 				});
 			},	
 			chapters4:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:4},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:4},
 				{emulateJSON:true})
 				.then(res => {
 	//			this.list = (res.body.ResultDate.bookInfo);
@@ -320,7 +309,7 @@
 				});
 			},
 			chapters5:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:5},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:5},
 				{emulateJSON:true})
 				.then(res => {
 	//			this.list = (res.body.ResultDate.bookInfo);
@@ -334,7 +323,7 @@
 			},	
 			//分页
 			next:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:this.chapter_id},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:this.chapter_id},
 				{emulateJSON:true}
 				).then(res => {
 //				this.chapter_id =(res.body.ResultDate.chapterInfo.chapter_id);
@@ -342,19 +331,19 @@
 				this.chapter = (res.body.ResultDate.chapterInfo);
 //				console.log(this.chapter_id)
 				this.top = document.getElementById('main-wrap-big').scrollTop=300;
-//				this.top = 0;
-				console.log(this.top)
 				},err => {
 //					console.log('err');		
 				});
 			},
 			back:function(){
-				this.$http.post('http://47.94.240.34/chapter/',{book_id:1,chapter_id:this.chapter_id},
+				this.$http.post('http://47.94.240.34/chapter/',{book_id:3,chapter_id:this.chapter_id},
 				{emulateJSON:true})
 				.then(res => {
+	//			this.list = (res.body.ResultDate.bookInfo);
 				this.chapter_id =this.chapter_id-1;
 				this.chapter = (res.body.ResultDate.chapterInfo);
 				this.top = document.getElementById('main-wrap-big').scrollTop=300;
+//				console.log(this.chapter_id)
 					},err => {
 				//alert('wrong');
 					console.log('err');		
@@ -408,7 +397,6 @@
 					this.flag = false;
 				}
 			},
-			//facebook 
 			face:function() {
 				let reg = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
 				let mail = document.getElementById('text').value;
@@ -416,7 +404,7 @@
 				if(reg.test(mail)) {
 //					alert('ok');
 					console.log(mail)
-					this.$http.post('http://47.94.240.34/subscibe/',{book_id:this.detailId,chapter_id:this.chapter_id,email:mail,type:'facebook'},{emulateJSON:true}
+					this.$http.post('http://47.94.240.34/subscibe/',{book_id:this.detailId,chapter_id:this.chapetr_id,email:mail,type:'facebook'},{emulateJSON:true}
 				    ).then(res => {
 					console.log(res);
 					},err => {
@@ -465,16 +453,15 @@
 <style scoped>
 	textarea{
 		width: 9.383333rem;
-    	/*height: 10rem;*/
+    	height: 12rem;
     	resize: none;
     	border: none;
     	outline: none;
     	color: rgba(0, 0, 0, 0.87);
-		font-size:0.28385rem;
+		font-size:0.34385rem;
 		font-family: Microsoft YaHei;
 		text-align:justify;
 		text-justify:distribute;
-		/*overflow-y: hidden;*/
 	}
 	*{
 		font-family: "Microsoft YaHei";
@@ -498,7 +485,7 @@
 	
 	#header {
 		width: 10rem;
-		height: 100%;
+		/*height: 8.333333rem;*/
 		height: 1.694444rem;
 		line-height: 1.694444rem;
 		text-align: center;
@@ -529,6 +516,7 @@
 		margin: auto;
 	}
 	
+
 	
 	#Directory-options {
 		width: 8.333333rem;
@@ -583,12 +571,12 @@
 	
 	#main-text {
 		width: 9.383333rem;
-		/*height: 100%;*/
+		height: 100%;
 		margin: auto;
 		/*text-align: center;*/
 		color: rgba(0, 0, 0, 0.87);
 		font-size:0.230rem;
-		font-family: Microsoft YaHei;
+		/*font-family: "微软雅黑";*/
 		text-align:justify;
 		text-justify:distribute;
 		}
@@ -606,6 +594,7 @@
 		font-size:0.145rem;
 		font-family: Microsoft YaHei;
 	}
+	
 	#main-bottom {
 		width: 3.8rem;
 		height: 0.933333rem;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/i22'
+import Novel from '@/components/novel'
+import Novel2 from '@/components/novel2'
 
 Vue.use(Router)
 
@@ -10,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+    	path: '/novel',
+    	component: Novel
+    },{
+    	path: '/novel2',
+    	component: Novel2    	
     }
   ]
 })
